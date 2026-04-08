4ac389528faf7f73ec7a25da8747a2264c743c77
Daniel Castanho <Danielcastanho2000@gmail.com>
2026-04-08 14:35:06 +0100

    Resoluçao disponibilidade

--- app/dashboard/_data/formando.ts
+++ app/dashboard/_data/formando.ts
@@ -55,7 +55,6 @@ export async function getFormandoStats(userId: string) {
         pendingInvitations: convitesPendentes,
     };
 }
-}
 
 // ------------------- CURSO DO FORMANDO -------------------
 export async function getCursoFormando(userId: string) {
