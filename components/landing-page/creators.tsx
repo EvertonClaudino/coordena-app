d7a106aaf3b4044569afc3aab7dba26d367b60ff
mp-24 <margarida.pais95@gmail.com>
2026-04-12 11:44:24 +0100

    cards 2x2 mobile: sobre nos; funcionalidades; criadores

--- components/landing-page/creators.tsx
+++ components/landing-page/creators.tsx
@@ -59,7 +59,7 @@ export function Creators() {
                     </h2>
                 </div>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
                     {creators.map((creator, index) => (
                         <motion.div
                             key={creator.name}
