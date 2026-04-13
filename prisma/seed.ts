dd7c310524c8e834c6841fa40522fac22b4d1ae8
EvertonClaudino <evertonclaudinoit@gmail.com>
2026-04-13 11:33:45 +0100

    Adicionado acoordion

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,19 +4,6 @@ import bcrypt from 'bcryptjs'
 const prisma = new PrismaClient()
 
 async function main() {
-    // Coordenador
-    await prisma.user.upsert({
-        where: { email: 'coordenador@coordena.pt' },
-        update: {},
-        create: {
-            nome: 'Ana',
-            email: 'coordenador@coordena.pt',
-            senha: await bcrypt.hash('123456', 10),
-            role: 'COORDENADOR',
-            coordenador: { create: {} },
-        },
-    })
-
 
     console.log('✅ Seed concluído!')
 }
